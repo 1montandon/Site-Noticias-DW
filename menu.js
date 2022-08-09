62f769b63d33a078978300dba5fdd2986ec59d91
= <=>
2022-08-09 15:31:28 -0300

    Criação de menu retratio

---
+++ menu.js
@@ -0,0 +1,9 @@
+function remove(){
+    document.getElementById(`sidebar`).classList.remove(`active`);
+    
+} 
+
+function show(){
+    document.getElementById(`sidebar`).classList.add(`active`);
+}
+
