6d1454765f1697154b2ef2d9d4f4d902de1e5597
= <=>
2022-08-18 12:53:33 -0300

    Alterção nas imagens (js css )

--- menu.js
+++ menu.js
@@ -1,9 +1,21 @@
 function remove(){
     document.getElementById(`sidebar`).classList.remove(`active`);
+    let elementos = document.getElementsByClassName(`testando`)
+    for (const el of elementos) {
+        el.classList.add(`xxxxx`);
+    }
     
 } 
 
 function show(){
     document.getElementById(`sidebar`).classList.add(`active`);
+
+    let elementos = document.getElementsByClassName(`testando`)
+    for (const el of elementos) {
+        el.classList.remove(`xxxxx`);
+    }
+   
+
 }
+
 
